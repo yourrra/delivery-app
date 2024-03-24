c2cc6ec1b24d653e5392b4ed05fdb2723444d72a
yourrra <yuri15616@gmailcom>
2024-03-24 12:51:28 +0300

    add new component Link

--- src/pages/main/Main.tsx
+++ src/pages/main/Main.tsx
@@ -1,3 +1,16 @@
+import { Link } from '@/components/Link'
+import { Typography } from '@/components/typography'
+import { ROUTES } from '@/constants/urls'
+
 export const Main = () => {
-  return <div>Main</div>
+  return (
+    <div>
+      <Typography tag="h1" variant="h1">
+        Main
+      </Typography>
+      <Link type="link" props={{ to: ROUTES.MENU }}>
+        Menu
+      </Link>
+    </div>
+  )
 }
